058c779f632240a6604f8345bdca1bf26882b0ea
Alfathurmuhamadzidan <92446898+Alfathurmuhamadzidan@users.noreply.github.com>
2022-03-05 21:44:34 +0700

    Add files via upload

---
+++ praktikum1/VariabelUser.php
@@ -0,0 +1,15 @@
+<?php
+    // definisika variabels
+    $nama = "Alfathur Muhamad Zidan" ;
+    $umur = 18;
+    $berat = 41;
+
+    echo "Nama : " . $nama;
+    echo "<br/> Umur : " . $umur . " Tahun";
+    echo "<br/> Berat Badan : " . $berat . " kg";
+
+    echo "<br/>hallo $nama apakabar ?";
+
+
+
+?>
